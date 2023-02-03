95878ed192f3b7f6d355163ba140e0aadbdcc763
ElenaPhoenix <sglena@mail.ru>
2023-02-03 22:38:47 +0300

    added Seminar_1\Task_4.py

---
+++ Seminar_1/Task_4.py
@@ -0,0 +1,23 @@
+#Дано натуральное число. Требуется определить, является ли год с данным номером высокосным. Если год является высокосным, то выведите
+#YES, иначе выведите NO.(Год является высокосным, если его номер кратен 4 или 400, но не кратен 100 )
+#Input:2016
+#Output:YES
+import os
+os.system('cls')                                        #Самоочистка терминала
+def num_input(invite_msg):                              #Функция проверки ввода значений
+    user_input=input(invite_msg)
+    try:
+        return(int(user_input))
+    except:
+        print('Нужно ввести целое число. Попытайтесь снова')
+        return num_input(invite_msg)
+
+year=int(input('Введите номер года: '))
+if year%400==0 or year%100!=0 and year %4==0:
+    print('YES')
+else:
+    print('NO')
+
+
+
+
