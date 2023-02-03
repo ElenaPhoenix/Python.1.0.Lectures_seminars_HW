d15c4f1a963185ef98fffd9654a01bd496fe9d6f
ElenaPhoenix <sglena@mail.ru>
2023-02-04 00:04:53 +0300

    added DZ_after_Sem1\Task_1.py

---
+++ DZ_after_Sem1/Task_1.py
@@ -0,0 +1,9 @@
+#Найдите сумму цифр трехзначного числа
+import os
+os.system('cls')                                        #Самоочистка терминала
+
+number=input('Введите трехзначное число: ')
+digit_1= int(number[0])
+digit_2 = int(number[1])
+digit_3 = int(number[2])
+print("Сумма цифр числа:", digit_1 + digit_2 + digit_3)
