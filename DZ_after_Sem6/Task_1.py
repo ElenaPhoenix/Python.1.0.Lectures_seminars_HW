1167418bc7ac8c7d47fb294014f7d3097b3910b9
Elena_Phoenix Home <latikayegane@yandex.ru>
2023-02-21 23:00:26 +0300

    added last changes

--- DZ_after_Sem6/Task_1.py
+++ DZ_after_Sem6/Task_1.py
@@ -5,12 +5,19 @@ import os
 os.system('cls')
 
 first_el=int(input('Введите первый элемент арифметической прогрессии: '))
-step=int(input('Введите шаг арифметической прогрессии: '))
+step=int(input('Введите разность арифметической прогрессии (шаг): '))
 quantity=int(input('Введите количество элементов арифметической прогрессии: '))
 
 my_list=[]
-for i in range(quantity):
+for i in range(quantity):              #или в list complications: my_list=[first_el+(i-1)*step for i in range(1, quantity+1)]
     my_list.append(first_el)
     first_el+=step
 print(*my_list)
 
+
+# Другой вариант решения:
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# for i in range(n):
+# print(a1 + i * d)
--- DZ_after_Sem6/Task_2.py
+++ DZ_after_Sem6/Task_2.py
@@ -1,5 +1,6 @@
 # Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше 
 # заданного максимума)
+
 from random import randint
 import os
 os.system('cls')
@@ -9,8 +10,43 @@ print(array)
 min_span=int(input('Введите диапазон чисел, минимальное значение: '))
 max_span=int(input('Введите диапазон чисел, максимальное значение: '))
 
-result=[]
-for i in array:
-    if i>=min_span and i<=max_span:
-        result.append(i)
-print(result)
+for i in range(len(array)):
+    if min_span<=array[i]<=max_span:
+        print(i)
+
+
+# Другой вариант решения:
+# from random import randint
+
+# def print_list_indexes(list0:list):
+#     for i in range(len(list0)):
+#         print(f'{i:3}', end=' ')
+#     print()
+
+# def print_list(list0:list):
+#     for i in list0:
+#         print(f'{i:3}', end=' ')
+#     print()
+
+# def range_list_indexes(list0:list, min0:int, max0:int):
+#     range_list_indexes0 = []
+#     for i in range(len(list0)):
+#         if min0<=list0[i]<=max0:
+#             range_list_indexes0.append(i)
+#     return range_list_indexes0
+
+# num=[randint(-10,10) for _ in range(20)]
+# print_list_indexes(num)
+# print_list(num)
+# min_span=int(input('Введите диапазон чисел, минимальное значение: '))
+# max_span=int(input('Введите диапазон чисел, максимальное значение: '))
+# range_num_indexes=range_list_indexes(num,min_span,max_span)
+# print('Индексы чисел, входящих в диапазон: ')
+# print(range_num_indexes)
+
+
+# Другой вариант решения:
+# from random import randint
+# start, stop=[int(i) for i in input('Enter range: ').split()]
+# list_num=[randint(-10,10) for _ in range(randint(10,20))]
+# print(list_num, [i for i, n in enumerate(list_num) if n in range(start,stop+1)], sep='\n')
